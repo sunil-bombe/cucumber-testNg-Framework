0632df384033f6660371a949b0e277fe4e28dd65
Sunil Bombe <sunil.bombe1998@gmail.com>
2026-07-09 19:55:40 +0530

    added parallel execution and failed rerun and added extent report

--- src/test/java/com/webwork/stepdefs/DashboardPageStepDefinition.java
+++ src/test/java/com/webwork/stepdefs/DashboardPageStepDefinition.java
@@ -1,26 +1,29 @@
 package com.webwork.stepdefs;
 
 import com.webwork.context.TestContextSetup;
+import com.webwork.pages.DashboardPage;
 import io.cucumber.java.en.Then;
 import org.testng.Assert;
 
 public class DashboardPageStepDefinition {
 
   private TestContextSetup testContextSetup;
+  private DashboardPage dashboardPage;
 
   public DashboardPageStepDefinition(TestContextSetup testContextSetup) {
     this.testContextSetup = testContextSetup;
+    this.dashboardPage = this.testContextSetup.pageObjectManager.getDashboardPage();
   }
 
   @Then("I should be redirected to the dashboard")
   public void i_should_be_redirected_to_the_dashboard() {
     Assert.assertTrue(
-        testContextSetup.pageObjectManager.getDashboardPage().verifyUserIsLandedOnDashboardPage());
+        dashboardPage.verifyUserIsLandedOnDashboardPage());
   }
 
   @Then("I should see a welcome message")
   public void i_should_see_a_welcome_message() {
     Assert.assertTrue(
-        testContextSetup.pageObjectManager.getDashboardPage().verifyTheWelcomeMessage());
+        dashboardPage.verifyTheWelcomeMessage());
   }
 }
--- src/test/java/com/webwork/stepdefs/Hooks.java
+++ src/test/java/com/webwork/stepdefs/Hooks.java
@@ -1,10 +1,18 @@
 package com.webwork.stepdefs;
 
+import com.aventstack.extentreports.reporter.FileUtil;
 import com.webwork.constants.GlobalConstants;
 import com.webwork.context.TestContextSetup;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import java.io.File;
 import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 public class Hooks {
 
@@ -14,14 +22,24 @@ public class Hooks {
     this.testContextSetup = testContextSetup;
   }
 
-  @Before()
+  @Before
   public void beforeScenario() throws IOException {
     var url = testContextSetup.propertyFileUtils.getPropertyValue(GlobalConstants.GLOBAL_CONFIG_PROPERTIES_FILE_NAME, GlobalConstants.BROWSER_NAME_KEY);
     testContextSetup.webDriverManager.getWebDriver().get("https://opensource-demo.orangehrmlive.com");
   }
 
-  @After()
+  @After
   public void afterScenario() {
     testContextSetup.webDriverManager.quitDriver();
+  }
+
+  @AfterStep
+  public void afterStepMethod(Scenario scenario) throws IOException {
+    WebDriver driver = testContextSetup.webDriverManager.getWebDriver();
+    if(scenario.isFailed()){
+      File sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+      byte[] fileContent = FileUtils.readFileToByteArray(sourcePath);
+      scenario.attach(fileContent, "image/png", "image");
+    }
   }
 }
